303f7b21c0042da93fbb337ad88f58f035ae5987
Yeo Seongil <seongil_yeo@naver.com>
2025-04-21 15:52:24 +0900

    [#3]Rename: 파일구조 수정

--- Challenge2_Sticky/Soruce/Data/Enum/TextFieldType.swift
+++ Challenge2_Sticky/Soruce/Data/Enum/TextFieldType.swift
@@ -7,11 +7,13 @@
 
 enum TextFieldType {
     case title
+    case place
     case content
     
     var maxLength: Int {
         switch self {
         case .title: return 15
+        case .place: return 10
         case .content: return 100
         }
     }
