68cd0b5e89a8cf33f4ea57aa158ed41d461148f0
Yeo Seongil <seongil_yeo@naver.com>
2025-04-21 15:58:22 +0900

    [#3]Feat: SwiftData ModelContainer 주입

--- Challenge2_Sticky/Soruce/App/Challenge2_StickyApp.swift
+++ Challenge2_Sticky/Soruce/App/Challenge2_StickyApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Challenge2_StickyApp: App {
+    var stickyModelContrainer: ModelContainer = {
+        let schema = Schema([
+            Sticky.self,
+            StickerBook.self,
+            Sticker.self
+        ])
+        
+        let modelConfiguration = ModelConfiguration(schema: schema,
+                                                    isStoredInMemoryOnly: false)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("ModelContainer Error \(error)")
+        }
+    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(stickyModelContrainer)
     }
 }
