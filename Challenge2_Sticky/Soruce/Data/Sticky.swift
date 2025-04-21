667674c173a92b405b64ce8cffb4c704aeafa765
Yeo Seongil <seongil_yeo@naver.com>
2025-04-21 15:59:45 +0900

    [#3]Comment: 주석 삭제

--- Challenge2_Sticky/Soruce/Data/Sticky.swift
+++ Challenge2_Sticky/Soruce/Data/Sticky.swift
@@ -8,12 +8,6 @@
 import SwiftUI
 import SwiftData
 
-// Q?
-// 각 스티키마다 스티커북이 있음
-// 스티커는 총 8개이고, isSelected 변수를 통해 선택 됐는지, 안됐는지 관리함
-// 모델이 두 개여야 하는지? 관계 설정을 해야하는지?
-
-// Sticker1 ~ 8까지 변수를 만들고, isSelected를 관리할지?
 @Model
 final class Sticky {
     @Attribute(.unique) var id: UUID
